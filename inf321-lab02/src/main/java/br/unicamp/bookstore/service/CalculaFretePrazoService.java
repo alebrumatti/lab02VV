9eee48e9c1c19af7c2585bb9a7dec84f7114458f
esoft51 - Rharamys Bordini <rharamys@hotmail.com>
2017-10-12 10:13:11 -0300

    inclusao

--- inf321-lab02/src/main/java/br/unicamp/bookstore/service/CalculaFretePrazoService.java
+++ inf321-lab02/src/main/java/br/unicamp/bookstore/service/CalculaFretePrazoService.java
@@ -1,5 +1,14 @@
 package br.unicamp.bookstore.service;
 
+import br.unicamp.bookstore.model.PrecoPrazo;
+import br.unicamp.bookstore.model.Produto;
+import br.unicamp.bookstore.model.TipoEntregaEnum;
+
 public class CalculaFretePrazoService {
 
+	public PrecoPrazo calcular(Produto produto, TipoEntregaEnum tipoEntregar) throws Exception {
+	    String url = String.format("%s/%s/xml", configuracao.getBuscarEnderecoUrl(), cep);
+	    return new RemoteService().getAndParseXml(url, Endereco.class);
+	  }
+	
 }
